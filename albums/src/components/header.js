2af942cae63e405010beb81fe90d119300a0646b
Zane <webmaster@zaneweb.org>
2017-11-05 09:00:16 -0800

    Add basic styling

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -3,7 +3,18 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // Make a component
-const Header = () => <Text>Albums</Text>;
+const Header = () => {
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums</Text>;
+};
+
+// Styling
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 // Make the component available to other parts of the app
 export default Header;
