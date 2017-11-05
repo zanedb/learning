01eacad17d003104e3cb8b7eaeab9cd7465850df
Zane <webmaster@zaneweb.org>
2017-11-05 10:32:57 -0800

    Basic GET request with Axios

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  // Runs when component is about to be rendered
   componentWillMount() {
-    // Runs when component is about to be rendered
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
   }
 
+  // Renders data on device
   render() {
     return (
       <View>
