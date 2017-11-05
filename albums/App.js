e93a17bb16e12c8caf64371d09a9fe1eaf125dbe
Zane <webmaster@zaneweb.org>
2017-11-05 08:05:46 -0800

    Add header

--- albums/App.js
+++ albums/App.js
@@ -1,12 +1,14 @@
 // Import a library to help create a Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a Component
 const App = () => (
-  <Text>Some Text</Text>
+  <Header />
 );
 
+// Export default
 export default App;
 
 // Render it to the device
