979661a93f37e1d74a7057859980430e2e55e5e0
Zane <webmaster@zaneweb.org>
2017-11-19 11:04:45 -0800

    better organization

---
+++ react-native/albums/App.js
@@ -0,0 +1,19 @@
+// Import libraries
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create components
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Export default
+export default App;
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
