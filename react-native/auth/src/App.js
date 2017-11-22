65f4d423a3615dd6a23e5d4ff5c1d9607429a0fb
Zane <webmaster@zaneweb.org>
2017-11-21 22:11:14 -0800

    config ESLint and add firebase

--- react-native/auth/src/App.js
+++ react-native/auth/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount() {
+
+  }
+
   render() {
     return (
       <View>
