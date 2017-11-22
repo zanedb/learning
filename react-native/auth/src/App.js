20514191226818aab1cb7a53ae8e2dcaa7cb20d0
Zane <webmaster@zaneweb.org>
2017-11-22 12:41:19 -0800

    bugfix

--- react-native/auth/src/App.js
+++ react-native/auth/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
-import { LoginForm } from './components/LoginForm';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
